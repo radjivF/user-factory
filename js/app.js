51f682d60618b8bc4ca6c98251b5af61aee864c0
radjiv fender <carrereradjiv@yahoo.fr>
2015-04-10 17:06:21 +0200

    add nwjs

---
+++ js/app.js
@@ -0,0 +1,44 @@
+var im = require('imagemagick');
+var path = require ('path')
+
+window.ondragover = function(e){
+	e.preventDefault(); return false;
+}
+
+window.ondrop = function(e){
+	e.preventDefault(); return false;	
+}
+
+var el = document.querySelector('#drop');
+el.ondragover = function(){
+	this.className ="hover";
+	this.innerHTML ="Glisse ton fichier";
+	return false;
+}
+
+el.ondragleave = function(){
+	this.className = "";
+	this.innerHTML = "Dépose ton fichier excel";
+	return false;
+}
+
+el.ondrop = function(e){
+	e.preventDefault();
+	for(var i=0; i < e.dataTransfer.files.length; ++i){
+		var file= e.dataTransfer.files[i].path;
+		console.log(path.extname(file));
+		if(path.extname(file)== ".xlsx"){
+			el.className =  "";
+			el.innerHTML ="travail terminé";
+			var audio  = new Audio('sound/done.ogg');
+			audio.volume= 0.2;
+			audio.play();
+		}
+		else{
+			el.style.color = 'red';
+			el.innerHTML ="Fichier excel requis";			
+		}
+		
+	}
+}
+
