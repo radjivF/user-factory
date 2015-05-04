275d4fc861473f257b932b23c1334b49334ef6e0
radjiv fender <carrereradjiv@yahoo.fr>
2015-05-04 13:17:42 +0200

    update gitignore

---
+++ app/template/template-basic.js
@@ -0,0 +1,18 @@
+
+var user = {
+    "provider" : "",
+    "providerUserId" : "",
+    "displayName" : "",
+    "login" : "",
+    "password" : "",
+    "passwordHashed": "false",
+    "gender" : "male",
+    "role" : [],
+    "name" : {
+        "familyName" : "",
+        "givenName" : ""
+
+    }
+};
+
+exports.user = user;
---
+++ app/template/template-langue.js
@@ -0,0 +1,20 @@
+
+var user = {
+    
+    "provider" : "",
+    "providerUserId" : "",
+    "displayName" : "",
+    "login" : "",
+    "password" : "",
+    "language":"",
+    "passwordHashed": "false",
+    "gender" : "male",
+    "role" : [],
+    "name" : {
+        "familyName" : "",
+        "givenName" : ""
+
+    }
+};
+
+exports.user = user;
